4c177859f180d105acfaceebe9a4672bee9b6503
shhhQuiettt <krzysztof.skro@gmail.com>
2022-10-22 12:38:03 +0200

    Exporter added

--- python/tasker/src/json/Exporter.py
+++ python/tasker/src/json/Exporter.py
@@ -1,11 +1,11 @@
 import json
+from typing import List, Dict, Any
 
 
 class Exporter:
+    def __init__(self, tasks_destination: str = "taski.json"):
+        self.tasks_destination = tasks_destination
 
-    def __init__(self):
-        pass
-
-    def save_tasks(self, tasks):
-        # TODO zapisz taski do pliku tutaj
-        pass
+    def save_tasks(self, tasks: List[Dict[str, Any]]) -> None:
+        with open(self.tasks_destination, "w") as json_file:
+            json.dump(tasks, json_file)
