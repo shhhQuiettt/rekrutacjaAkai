8b85b8a6a9200d60ed6755dd4a9c9adc6b2d9520
shhhQuiettt <krzysztof.skro@gmail.com>
2022-10-22 12:29:00 +0200

    Importer added

--- python/tasker/src/json/Importer.py
+++ python/tasker/src/json/Importer.py
@@ -1,15 +1,15 @@
 import json
+from typing import List, Dict, Any
 
 
 class Importer:
+    def __init__(self, tasks_source: str = "taski.json"):
+        self.tasks = []
+        self.tasks_source = tasks_source
 
-    def __init__(self):
-        pass
+    def read_tasks(self) -> None:
+        with open(self.tasks_source) as json_file:
+            self.tasks = json.load(json_file)
 
-    def read_tasks(self):
-        # TODO odczytaj plik i zdekoduj treść tutaj
-        pass
-
-    def get_tasks(self):
-        # TODO zwróć zdekodowane taski tutaj
-        pass
+    def get_tasks(self) -> List[Dict[str, Any]]:
+        return self.tasks
