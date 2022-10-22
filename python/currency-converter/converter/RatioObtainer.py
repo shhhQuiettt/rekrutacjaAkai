d3418f5f62b0a468d889e658adb265a6294b49d4
shhhQuiettt <krzysztof.skro@gmail.com>
2022-10-22 14:06:36 +0200

    RatioObtainer created

--- python/currency-converter/converter/RatioObtainer.py
+++ python/currency-converter/converter/RatioObtainer.py
@@ -1,4 +1,7 @@
-import json, datetime, urllib.request
+import json, urllib.request
+from typing import Any, Dict, Optional
+from datetime import datetime
+from pprint import pprint
 
 
 class RatioObtainer:
@@ -8,29 +11,70 @@ class RatioObtainer:
     def __init__(self, base, target):
         self.base = base
         self.target = target
+        self.exchange_ratio: Optional[float] = None
+        self.cached_ratios = []
 
-    def was_ratio_saved_today(self):
-        # TODO
-        # This function checks if given ratio was saved today and if the file with ratios is created at all
-        # should return false when file doesn't exist or if there's no today's exchange rate for given values at all
-        # should return true otherwise
-        pass
+    def was_ratio_saved_today(self) -> bool:
+        try:
+            with open("ratios.json") as ratios_file:
+                self.cached_ratios = json.load(ratios_file)
+        except FileNotFoundError:
+            return False
+
+        for ratio in self.cached_ratios:
+            if (
+                self._is_the_same_ratio(ratio)
+                and datetime.strptime(ratio["date_fetched"], "%Y-%m-%d")
+                == datetime.today()
+            ):
+                self.exchange_ratio = ratio.ratio
+                return True
+
+        return False
+
+    def _is_the_same_ratio(self, ratio: Dict[str, Any]):
+        return (
+            ratio["base_currency"] == self.base
+            and ratio["target_currency"] == self.target
+        )
 
     def fetch_ratio(self):
-        # TODO
-        # This function calls API for today's exchange ratio
-        # Should ask API for today's exchange ratio with given base and target currency
-        # and call save_ratio method to save it
-        pass
+        with urllib.request.urlopen(
+            f"https://api.exchangerate.host/convert?from={self.base}&to={self.target}"
+        ) as response:
+            body = response.read()
 
-    def save_ratio(self, ratio):
-        # TODO
-        # Should save or update exchange rate for given pair in json file
-        # takes ratio as argument
-        # example file structure is shipped in project's directory, yours can differ (as long as it works)
-        pass
+        json_body = json.loads(body)
+        if json_body["success"] is False or json_body["result"] is None:
+            raise ValueError(f"Convertion not found for pair {self.base}-{self.target}")
+        new_ratio = self._normalize_json_response(json_body)
 
-    def get_matched_ratio_value(self):
-        # TODO
-        # Should read file and receive exchange rate for given base and target currency from that file
-        pass
+        self.exchange_ratio = new_ratio["ratio"]
+
+        self.save_ratio(new_ratio)
+
+    @staticmethod
+    def _normalize_json_response(data: Dict[str, Any]) -> Dict[str, Any]:
+        cleaned_data = {
+            "base_currency": data["query"]["from"],
+            "target_currency": data["query"]["to"],
+            "ratio": data["result"],
+            "date_fetched": data["date"],
+        }
+        return cleaned_data
+
+    def save_ratio(self, ratio: Dict[str, Any]) -> None:
+        existed = False
+        for i in range(len(self.cached_ratios)):
+            if self._is_the_same_ratio(self.cached_ratios[i]):
+                self.cached_ratios[i] = ratio
+                existed = True
+            break
+        if not existed:
+            self.cached_ratios.append(ratio)
+
+        with open("ratios.json", "w") as ratios_file:
+            json.dump(self.cached_ratios, ratios_file)
+
+    def get_matched_ratio_value(self) -> float:
+        return self.exchange_ratio
